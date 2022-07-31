5fff19b038836dae3d55eb26c432ecffb5b2daeb
h0rak <trey.horak@gmail.com>
2022-07-31 17:41:37 -0500

    Initial Commit

---
+++ Library/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Library
+//
+//  Created by Raymond Horak on 7/31/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
